81bcf3c4ab9f05341a0ca725062249c2a2dcf230
fjw_Code <1748202178@qq.com>
2018-05-06 18:50:15 +0800

    Main function.

--- auto_answer.py
+++ auto_answer.py
@@ -8,16 +8,16 @@ from ulits import fetch_que_ans, preprocess
 
 driver = AndroidDriver()
 
-num = 1
+num = 1   # answer from the first question or you decide.
 
 for i in islice(fetch_que_ans(), num - 1, None):
-    print(num)
-    driver.pull_screencap()
+    print(f'You are doing question {num}.')
+    driver.screencap_exec()
     positions = preprocess()
     for j in i:
-        driver.input_tap(140, positions[j])
-        # sleep(0.5)
+        driver.click(140, positions[j])
+        sleep(0.5)
     if num > 60:
-        driver.input_tap(888, 1800)
+        driver.click(888, 1800)
     num += 1
-    sleep(0.5)
+    sleep(3)
